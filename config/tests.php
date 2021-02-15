5bd050bc3571195a5a159d2a353530a05d931550
Anton Samoylenko <a.samoylenko95@gmail.com>
2021-02-15 14:07:47 +0200

    Fix test configuration

--- config/tests.php
+++ config/tests.php
@@ -6,12 +6,16 @@ use Psr\Container\ContainerInterface;
 use Psr\EventDispatcher\EventDispatcherInterface;
 use Psr\Log\LoggerInterface;
 use Yiisoft\Aliases\Aliases;
+use Yiisoft\EventDispatcher\Dispatcher\Dispatcher;
+use Yiisoft\EventDispatcher\Provider\Provider;
 use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\I18n\MessageFormatterInterface;
+use Yiisoft\Log\Logger;
 use Yiisoft\Router\UrlGeneratorInterface;
 use Yiisoft\View\Theme;
 use Yiisoft\View\View;
 use Yiisoft\View\ViewContextInterface;
+use Yiisoft\View\WebView;
 use Yiisoft\Yii\DataView\BaseListView;
 use Yiisoft\Yii\DataView\Columns\Column;
 use Yiisoft\Yii\DataView\DetailView;
@@ -53,13 +57,19 @@ return [
         '@basePath' => '@public',
         '@view' => '@public/view',
     ],
-    View::class => static function (ContainerInterface $container) {
+    LoggerInterface::class => static function (ContainerInterface $container) {
+        return new Logger();
+    },
+    EventDispatcherInterface::class => static function (ContainerInterface $container) {
+        return new Dispatcher($container->get(Provider::class));
+    },
+    WebView::class => static function (ContainerInterface $container) {
         $aliases = $container->get(Aliases::class);
         $eventDispatcher = $container->get(EventDispatcherInterface::class);
         $theme = $container->get(Theme::class);
         $logger = $container->get(LoggerInterface::class);
 
-        return new View($aliases->get('@view'), $theme, $eventDispatcher, $logger);
+        return new WebView($aliases->get('@view'), $theme, $eventDispatcher, $logger);
     },
     UrlGeneratorInterface::class => [
         '__class' => UrlGenerator::class,
